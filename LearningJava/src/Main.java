8b279d07876fc7efb1ffb6406e343f6a025e1223
rob <rob.77@live.com>
2020-08-10 10:00:37 -0500

    02 02

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args){
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
+
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
-
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
 
         System.out.println("StudentAge: " + studentAge);
         System.out.println("Student GPA: " + studentGPA);
