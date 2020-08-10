46870a05fdf0c386afc1cd2cb534d161563a915a
UHDrob <gomezr43@gator.uhd.edu>
2020-08-10 17:49:49 -0500

    05_02

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World from IntelliJ IDE !");
+        System.out.println("Hello World  IntelliJ IDE !");
     }
 }
