993b0d55d98aea768b79e708782b6981fa8ad4dc
rob <rob.77@live.com>
2020-08-10 10:37:44 -0500

    02 05

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
-    public static void main(String[] args){
-        int studentAge = 15;
+    public static void main(String[] args) {
         double studentGPA = 3.45;
-        boolean hasPerfectAttendance = true;
 
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
-
+        System.out.println(studentFirstName + " " + studentLastName);
 
     }
 }
