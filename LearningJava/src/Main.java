9119727182fea33f5ac60f66e3bf4ad2ec171957
rob <rob.77@live.com>
2020-08-10 10:04:49 -0500

    02 05

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -6,16 +6,7 @@ public class Main {
 
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
 
-        System.out.println("StudentAge: " + studentAge);
-        System.out.println("Student GPA: " + studentGPA);
-        System.out.println("Student First Initial: " + studentFirstInitial);
-        System.out.println("Student Last Initial: " + studentLastInitial);
-        System.out.println("Student Attendance: " + hasPerfectAttendance);
-        System.out.println("Student First Name: " + studentFirstName);
-        System.out.println("Student Last Name: " + studentLastName);
 
     }
 }
