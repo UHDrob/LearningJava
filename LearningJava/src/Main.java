09b13b762b667a8d0464509eb029bdb9bc91e959
UHDrob <gomezr43@gator.uhd.edu>
2020-08-10 16:19:02 -0500

    03_07b

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        String question = "";
+        String choiceOne = "";
+        String choiceTwo = "";
+        String choiceThree = "";
 
+        String correctAnswer = choiceTwo;
+
+        // Write a print statement asking the question
+        // Write a print statement giving the asnwer choices
+
+        // Have the user input an answer
+        // Retrieve the user's input
+
+        // if the user's input matches the correctAnswer..
+        // then the user is correct and we want to print out a congrats message to the user.
+        // then the user is correct and we want to print out a congrats message to the user.
+
+        // if the user's input does not match the correctAnswer..
+        // then the user is incorrect and we want to print out a message saying that the user is
     }
 }
