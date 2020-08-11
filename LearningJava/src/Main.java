82ba5d6c81c601c101eee829d61cdc4b334214f3
UHDrob <gomezr43@gator.uhd.edu>
2020-08-11 06:58:04 -0500

    05_06b

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+
+
     public static void main(String[] args){
-        System.out.println("Hello World from IntelliJ IDE !");
-    }
+        System.out.println("and FINISH it!");
+            double result = Math.pow(2, 5);
+            System.out.println(result);
+
+        }
 }
