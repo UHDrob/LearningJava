b57c593902b4d688a08e4bed81c9c77857520ae1
UHDrob <gomezr43@gator.uhd.edu>
2020-08-11 08:05:33 -0500

    05_07b

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -4,7 +4,11 @@ public class Main {
         Triangle triangleA = new Triangle(15,8,15,8,17);
         Triangle triangleB =  new Triangle(3,2.598,3,3,3);
 
+        double triangleArea = triangleA.findArea();
+        System.out.println(triangleArea);
 
-        System.out.println("Hello World from IntelliJ IDE !");
+        System.out.println(triangleA.sideLenThree);
+        System.out.println(triangleB.base);
+        System.out.println("and THIS is how we do it!");
     }
 }
